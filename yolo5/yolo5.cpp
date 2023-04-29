f929a5390540a81e24308f6f837454dc814495e8
aa568551096 <568551096@qq.com>
2023-04-29 18:09:53 +0800

    加载dll 实现 检测成功

---
+++ yolo5/yolo5.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#pragma comment(lib, "myDetect.lib")
+extern "C" void PreDetch_();
+extern "C" void ImgDetch_(std::vector<cv::Rect> &rect, std::vector<float> &conf,
+	std::vector<float> &classId, cv::Mat &img);
+extern "C" void AfterDetch_();
+int main() 
+{
+	PreDetch_();
+	std::vector<cv::Rect> rects;
+	std::vector<float> conf, classId;
+	cv::Mat img = cv::imread("7.png");
+	if (img.data == nullptr) {
+		std::cout << "open file fail" << std::endl;
+		return 0;
+	}
+	ImgDetch_(rects, conf, classId, img);
+	AfterDetch_();
+	return 0;
+}
